cab7b2a19d45b0ad3aeb42acac5144fdb568f019
Brittni Watkins <blwatkins@smu.edu>
2018-01-13 19:17:06 -0600

    Add initial file for Polygon class. Add base functions in main tab.

---
+++ MovingShapes/MovingShapes.pde
@@ -0,0 +1,15 @@
+// Moving Shapes
+// Multiple shapes moving around the screen
+
+void settings() {
+  
+}
+
+void setup() {
+  
+}
+
+
+void draw() {
+  
+}
---
+++ MovingShapes/Polygon.java
@@ -0,0 +1,20 @@
+// Moving Shapes
+// Multiple shapes moving around the screen
+// Polygon class 
+
+import processing.core.*;
+
+public class Polygon {
+
+  public enum ShapeType {
+    POLYGON, 
+      STAR
+  };
+
+  public enum DrawingType {
+    SOLID, 
+      WIREFRAME, 
+      TRANSPARENT
+  };
+  
+}
