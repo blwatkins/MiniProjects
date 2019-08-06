d0d09decc20241f6e71b5ed0b3942129570b394b
Brittni Watkins <blwatkins@smu.edu>
2019-08-06 17:53:36 -0500

    Add initial DrawingWithNoise code. Draw line from pmouse x,y to current mouse x,y

--- Drawing_with_Noise/src/DrawingWithNoise.java
+++ Drawing_with_Noise/src/DrawingWithNoise.java
@@ -1,2 +1,27 @@
-public class DrawingWithNoise {
+import processing.core.PApplet;
+
+public class DrawingWithNoise extends PApplet{
+
+    public static void main(String[] passedArgs) {
+        String[] appletArgs = new String[] { "DrawingWithNoise" };  // first string MUST match name of class
+
+        if (passedArgs != null) {
+            PApplet.main(concat(appletArgs, passedArgs));
+        } else {
+            PApplet.main(appletArgs);
+        }
+    }
+
+    public void settings() {
+        size(800, 800);
+    }
+
+    public void setup() {
+
+    }
+
+    public void draw() {
+        line(pmouseX, pmouseY, mouseX, mouseY);
+    }
+
 }
