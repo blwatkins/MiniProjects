2743d860198fc9718ff551ba2004bbea6926e9cb
Brittni Watkins <blwatkins@smu.edu>
2019-08-06 18:09:48 -0500

    Add point constructors and display method

--- Drawing_with_Noise/src/Point.java
+++ Drawing_with_Noise/src/Point.java
@@ -1,2 +1,29 @@
+// Point Class
+
+import processing.core.PApplet;
+import processing.core.PVector;
+
 public class Point {
+    PApplet p;
+    PVector position;
+    float diameter = 2;
+
+    public Point(PApplet p) {
+        this.p = p;
+        position = new PVector();
+    }
+
+    public Point(PApplet p, float x, float y) {
+        this.p = p;
+        position = new PVector(x, y);
+    }
+
+    public Point(PApplet p, PVector position) {
+        this.p = p;
+        this.position = position.copy();
+    }
+
+    public void display() {
+        p.ellipse(position.x, position.y, diameter, diameter);
+    }
 }
