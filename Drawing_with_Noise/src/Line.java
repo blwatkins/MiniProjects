6c7ce1e0c94d3919c8df8d472d5890f94280a2f4
Brittni Watkins <blwatkins@smu.edu>
2019-08-07 19:30:30 -0500

    Add constructors

--- Drawing_with_Noise/src/Line.java
+++ Drawing_with_Noise/src/Line.java
@@ -1,2 +1,32 @@
+import processing.core.PApplet;
+import processing.core.PVector;
+
 public class Line {
+
+    private PApplet p;
+    private PVector start;
+    private PVector end;
+
+    public Line(PApplet p) {
+        init(p);
+    }
+
+    public Line(PApplet p, PVector start, PVector end) {
+        init(p);
+        start.set(start);
+        end.set(end);
+    }
+
+    public Line(PApplet p, Point start, Point end) {
+       init(p);
+       this.start.set(start.getPosition());
+       this.end.set(end.getPosition());
+    }
+
+    private void init(PApplet p) {
+        this.p = p;
+        start = new PVector();
+        end = new PVector();
+    }
+
 }
