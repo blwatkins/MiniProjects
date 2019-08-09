174f2f14aa2262f1cd96bb79721f34b14c2cdd96
Brittni Watkins <blwatkins@smu.edu>
2019-08-09 17:22:14 -0500

    Fix slider type name in main class
    #8

--- Color_Calculator/src/ColorCalculator.java
+++ Color_Calculator/src/ColorCalculator.java
@@ -5,7 +5,7 @@ import processing.core.PApplet;
 import processing.core.PVector;
 
 public class ColorCalculator extends PApplet {
-    private Slider slider;
+    private HorizontalSlider slider;
 
     public static void main(String[] passedArgs) {
         String[] appletArgs = new String[] { "ColorCalculator" };  // first string MUST match name of class
@@ -24,7 +24,7 @@ public class ColorCalculator extends PApplet {
     public void setup() {
         float sliderLength = 400;
         PVector sliderPos = new PVector(width / 2 - sliderLength / 2, height / 2);
-        slider = new Slider(this, sliderPos, sliderLength, 20, 0, 60);
+        slider = new HorizontalSlider(this, sliderPos, sliderLength, 20, 0, 60);
     }
 
     public void draw(){
