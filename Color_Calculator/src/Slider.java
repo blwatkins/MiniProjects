b0527f22e5bafcb2d7f7f3867bccbd86191f1479
Brittni Watkins <blwatkins@smu.edu>
2019-08-09 16:50:21 -0500

    Add constructor for slider class #8

--- Color_Calculator/src/Slider.java
+++ Color_Calculator/src/Slider.java
@@ -1,2 +1,39 @@
+// Color Calculator
+// Slider Class
+
+import processing.core.PApplet;
+import processing.core.PVector;
+
 public class Slider {
+    private PApplet p;
+    private boolean active;
+    private PVector position;
+    private float length;
+    private float minValue;
+    private float maxValue;
+    private float currentValue;
+
+    public Slider(PApplet p) {
+        init(p);
+        position = new PVector();
+        length = 100;
+        minValue = 0;
+        maxValue = 0;
+        currentValue = minValue;
+    }
+
+    public Slider(PApplet p, PVector position, float length, float minValue, float maxValue) {
+        init(p);
+        this.position = position.copy();
+        this.length = length;
+        this.minValue = minValue;
+        this.maxValue = maxValue;
+        currentValue = minValue;
+    }
+
+    private void init(PApplet p) {
+        this.p = p;
+        active = false;
+    }
+
 }
