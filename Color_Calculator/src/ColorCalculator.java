32aad8df884b420ad1d0dfb88f74a4f594ae5e1b
Brittni Watkins <blwatkins@smu.edu>
2019-08-09 16:54:11 -0500

    Add slider object to main class #8

--- Color_Calculator/src/ColorCalculator.java
+++ Color_Calculator/src/ColorCalculator.java
@@ -2,8 +2,10 @@
 // Main Class
 
 import processing.core.PApplet;
+import processing.core.PVector;
 
 public class ColorCalculator extends PApplet {
+    private Slider slider;
 
     public static void main(String[] passedArgs) {
         String[] appletArgs = new String[] { "ColorCalculator" };  // first string MUST match name of class
@@ -20,11 +22,13 @@ public class ColorCalculator extends PApplet {
     }
 
     public void setup() {
-
+        float sliderLength = 400;
+        PVector sliderPos = new PVector(width / 2 - sliderLength / 2, height / 2);
+        slider = new Slider(this, sliderPos, sliderLength, 0, 60);
     }
 
     public void draw(){
-
+        slider.display();
     }
 
 }
