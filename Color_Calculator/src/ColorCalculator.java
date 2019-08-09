e2bd2c07fc844623179a825c283e23285d35cc05
Brittni Watkins <blwatkins@smu.edu>
2019-08-09 16:42:26 -0500

    Add initial code for opening Processing sketch #8

--- Color_Calculator/src/ColorCalculator.java
+++ Color_Calculator/src/ColorCalculator.java
@@ -1,2 +1,30 @@
-public class ColorCalculator {
+// Color Calculator
+// Main Class
+
+import processing.core.PApplet;
+
+public class ColorCalculator extends PApplet {
+
+    public static void main(String[] passedArgs) {
+        String[] appletArgs = new String[] { "ColorCalculator" };  // first string MUST match name of class
+
+        if (passedArgs != null) {
+            PApplet.main(concat(appletArgs, passedArgs));
+        } else {
+            PApplet.main(appletArgs);
+        }
+    }
+
+    public void settings() {
+        size(800, 800);
+    }
+
+    public void setup() {
+
+    }
+
+    public void draw(){
+
+    }
+
 }
