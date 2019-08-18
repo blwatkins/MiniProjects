8f22af53061b3c0a2e4c6e89c1eb28558f8a087e
Brittni Watkins <blwatkins@smu.edu>
2019-08-17 19:58:27 -0500

    Add processing startup code #11

--- ShapeRain/src/ShapeRain.java
+++ ShapeRain/src/ShapeRain.java
@@ -1,2 +1,30 @@
-public class ShapeRain {
+// Shape Rain
+// Main Class
+
+import processing.core.PApplet;
+
+public class ShapeRain extends PApplet {
+
+    public static void main(String[] passedArgs) {
+        String[] appletArgs = new String[] { "ShapeRain" };  // first string MUST match name of class
+
+        if (passedArgs != null) {
+            PApplet.main(concat(appletArgs, passedArgs));
+        } else {
+            PApplet.main(appletArgs);
+        }
+    }
+
+    public void settings() {
+        size(800, 800);
+    }
+
+    public void setup() {
+
+    }
+
+    public void draw(){
+
+    }
+
 }
