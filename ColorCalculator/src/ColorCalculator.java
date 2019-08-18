b6b66dc2ce6b859179afcde6f15305a0150349c8
Brittni Watkins <blwatkins@smu.edu>
2019-08-18 06:26:02 -0500

    Add interactive, scaling hsb sliders to program
    #8

--- ColorCalculator/src/ColorCalculator.java
+++ ColorCalculator/src/ColorCalculator.java
@@ -5,8 +5,10 @@ import processing.core.PApplet;
 import processing.core.PVector;
 
 public class ColorCalculator extends PApplet {
-    private HorizontalSlider slider;
-    private HorizontalSlider s;
+    private HorizontalSlider[] hsbSliders;
+    private HorizontalSlider[] rgbSliders;
+    private float sliderLength;
+    private float sliderHeight;
 
     public static void main(String[] passedArgs) {
         String[] appletArgs = new String[] { "ColorCalculator" };  // first string MUST match name of class
@@ -19,32 +21,58 @@ public class ColorCalculator extends PApplet {
     }
 
     public void settings() {
+        //fullScreen();
         size(800, 800);
     }
 
     public void setup() {
-        float sliderLength = 400;
-        PVector sliderPos = new PVector(width / 2f - sliderLength / 2, height / 2);
-        slider = new HorizontalSlider(this, sliderPos, sliderLength, 10, 0, 255);
-        s = new HorizontalSlider(this, sliderPos.sub(new PVector(0, 100)), sliderLength, 10, 60, 100);
+        sliderLength = (width / 2F) - (width / 8F);
+        sliderHeight = (height / 40F);
+        initHSBSliders();
     }
 
     public void draw(){
-        background(255);
-        if (slider.isActive()) {
-            float val = slider.getCurrentValue();
-            s.setCurrentValue(val / 2);
-        } else if (s.isActive()) {
-            float val = s.getCurrentValue();
-            slider.setCurrentValue(val * 2);
+        setHSBBackgroundColor();
+        for (int i = 0; i < hsbSliders.length; i++) {
+            hsbSliders[i].display();
         }
-        slider.display();
-        s.display();
+
     }
 
     public void mousePressed() {
-        slider.mousePressed(mouseX, mouseY);
-        s.mousePressed(mouseX, mouseY);
+        for (int i = 0; i < hsbSliders.length; i++) {
+            hsbSliders[i].mousePressed(mouseX, mouseY);
+        }
+    }
+
+    private void initHSBSliders() {
+        hsbSliders = new HorizontalSlider[3];
+        PVector sliderPos = new PVector(width / 16, 16 * height / 20);
+        for (int i = 0; i < hsbSliders.length; i++) {
+            hsbSliders[i] = new HorizontalSlider(this, sliderPos, sliderLength, sliderHeight, 0, 360);
+            sliderPos.add(new PVector(0, sliderHeight * 2));
+        }
+    }
+
+    private void setHSBBackgroundColor() {
+        int h = (int)hsbSliders[0].getCurrentValue();
+        int s = (int)hsbSliders[1].getCurrentValue();
+        int b = (int)hsbSliders[2].getCurrentValue();
+
+        colorMode(HSB, 360);
+        fill(color(h, s, b));
+        noStroke();
+        rect(0, 14 * height / 20, width / 2, height);
+        textSize(20);
+
+        if (b > 180) {
+            fill(0);
+        } else {
+            fill(255);
+        }
+
+        textAlign(CENTER, TOP);
+        text("H = " + h + "; S = " + s  + "; B = " + b, width / 4, 14 * height / 20);
     }
 
 }
