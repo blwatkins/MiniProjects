06ea1ffe4b42dd7c1c43999abd2b0472febc8bbb
Brittni Watkins <blwatkins@smu.edu>
2020-02-27 15:48:30 -0600

    Change map color to return random color from proper color generator type

--- HexagonOfCircles/src/color/RGB/RGBColorGenerator.java
+++ HexagonOfCircles/src/color/RGB/RGBColorGenerator.java
@@ -11,11 +11,7 @@ public abstract class RGBColorGenerator extends ColorGenerator {
     }
 
     public int mapColor(float value, float minValue, float maxValue, int saturation, int brightness) {
-        p.colorMode(p.RGB, 255);
-        int r = (int)p.random(255);
-        int g = (int)p.random(255);
-        int b = (int)p.random(255);
-        return p.color(r, g, b);
+        return randomColor();
     }
 
 }
