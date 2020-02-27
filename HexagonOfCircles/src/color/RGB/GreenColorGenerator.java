996961aa456fc12193576beadb0bd00377c8a69c
Brittni Watkins <blwatkins@smu.edu>
2020-02-27 15:33:24 -0600

    Complete GreenColorGenerator class

--- HexagonOfCircles/src/color/RGB/GreenColorGenerator.java
+++ HexagonOfCircles/src/color/RGB/GreenColorGenerator.java
@@ -8,4 +8,12 @@ public class GreenColorGenerator extends RGBColorGenerator {
         super(p);
     }
 
+    public int randomColor() {
+        p.colorMode(p.RGB, 255);
+        int r = (int)p.random(50, 100);
+        int g = (int)p.random(100, 255);
+        int b = (int)p.random(50, 100);
+        return p.color(r, g, b);
+    }
+
 }
