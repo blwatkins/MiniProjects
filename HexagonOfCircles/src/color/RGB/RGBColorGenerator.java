eb313354ad31c698f8d5ba9e274382ac828b0619
Brittni Watkins <blwatkins@smu.edu>
2020-02-27 15:41:17 -0600

    Remove old randomColor method from RGBColorGenerator

--- HexagonOfCircles/src/color/RGB/RGBColorGenerator.java
+++ HexagonOfCircles/src/color/RGB/RGBColorGenerator.java
@@ -4,7 +4,7 @@ import processing.core.PApplet;
 
 import color.ColorGenerator;
 
-public class RGBColorGenerator extends ColorGenerator {
+public abstract class RGBColorGenerator extends ColorGenerator {
 
     public RGBColorGenerator(PApplet p) {
         super(p);
@@ -15,48 +15,6 @@ public class RGBColorGenerator extends ColorGenerator {
         int r = (int)p.random(255);
         int g = (int)p.random(255);
         int b = (int)p.random(255);
-        return p.color(r, g, b);
-    }
-
-    public int randomColor() {
-        int r;
-        int g;
-        int b;
-        p.colorMode(p.RGB, 255);
-
-        switch (colorType) {
-            case RED:
-                r = (int)p.random(100, 255);
-                g = (int)p.random(50, 100);
-                b = (int)p.random(50, 100);
-                break;
-            case YELLOW:
-                r = (int)p.random(100, 255);
-                g = (int)p.random(100, 255);
-                b = (int)p.random(50, 100);
-                break;
-            case GREEN:
-                r = (int)p.random(50, 100);
-                g = (int)p.random(100, 255);
-                b = (int)p.random(50, 100);
-                break;
-            case CYAN:
-                r = (int)p.random(50, 100);
-                g = (int)p.random(100, 255);
-                b = (int)p.random(100, 255);
-                break;
-            case MAGENTA:
-                r = (int)p.random(100, 255);
-                g = (int)p.random(50, 100);
-                b = (int)p.random(100, 255);
-                break;
-            default:
-                r = (int)p.random(255);
-                g = (int)p.random(255);
-                b = (int)p.random(255);
-                break;
-        }
-
         return p.color(r, g, b);
     }
 
