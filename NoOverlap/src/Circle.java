fef4fb906f7588c298f4deab6156a60a053d43e4
Brittni Watkins <blwatkins@smu.edu>
2020-05-12 11:52:29 -0500

    Add basic methods for Circle class (constructor, display, overlap)

--- NoOverlap/src/Circle.java
+++ NoOverlap/src/Circle.java
@@ -1,2 +1,27 @@
+import processing.core.PApplet;
+import processing.core.PVector;
+
 public class Circle {
+    private PApplet p;
+    private PVector position;
+    private float radius;
+    private int color;
+
+    public Circle(PApplet p) {
+        this.p = p;
+        position = new PVector(p.random(p.width), p.random(p.height));
+        radius = p.random(5, 50);
+        color = p.color((int)p.random(255), (int)p.random(255), (int)p.random(255));
+    }
+
+    public void display() {
+        p.fill(color);
+        p.noStroke();
+        p.ellipse(position.x, position.y, radius * 2, radius * 2);
+    }
+
+    public boolean overlap(Circle c) {
+        float distance = PApplet.dist(position.x, position.y, c.position.x, c.position.y);
+        return distance < radius + c.radius;
+    }
 }
