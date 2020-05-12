6c6b15065622e98822b114a5a310980ef2952085
Brittni Watkins <blwatkins@smu.edu>
2020-05-12 12:23:56 -0500

    Add circles and overlap check

--- NoOverlap/src/NoOverlap.java
+++ NoOverlap/src/NoOverlap.java
@@ -1,6 +1,8 @@
 import processing.core.PApplet;
+import processing.core.PVector;
 
 public class NoOverlap extends PApplet {
+    private Circle[] circles;
 
     public static void main(String[] args) {
         String[] processingArgs = {"NoOverlap"};
@@ -12,11 +14,49 @@ public class NoOverlap extends PApplet {
     }
 
     public void setup() {
+        circles = new Circle[100];
 
+        for (int i = 0; i < circles.length; i++) {
+            circles[i] = new Circle(this);
+        }
+
+        checkOverlap();
     }
 
     public void draw() {
+        background(0);
 
+        for (Circle circle: circles) {
+            circle.display();
+        }
+    }
+
+    public void mousePressed() {
+
+    }
+
+    private void checkOverlap() {
+        boolean overlap;
+
+        do {
+            overlap = false;
+            for (Circle a : circles) {
+                for (Circle b : circles) {
+                    if (a != b) {
+                        if (a.overlap(b)) {
+                            overlap = true;
+                            removeOverlap(a, b);
+                        }
+                    }
+                }
+            }
+        } while (overlap);
+    }
+
+    private void removeOverlap(Circle a, Circle b) {
+        PVector diff = PVector.sub(a.getPosition(), b.getPosition());
+        diff.normalize().mult(a.getRadius() + b.getRadius());
+        b.getPosition().set(PVector.add(a.getPosition(), diff));
     }
 
 }
