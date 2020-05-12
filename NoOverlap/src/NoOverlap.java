993b2607aa312212960586b8b0525b69ce2b2648
Brittni Watkins <blwatkins@smu.edu>
2020-05-12 11:40:33 -0500

    Add basic Processing sketch structure

--- NoOverlap/src/NoOverlap.java
+++ NoOverlap/src/NoOverlap.java
@@ -1,2 +1,22 @@
-public class NoOverlap {
+import processing.core.PApplet;
+
+public class NoOverlap extends PApplet {
+
+    public static void main(String[] args) {
+        String[] processingArgs = {"NoOverlap"};
+        PApplet.main(processingArgs);
+    }
+
+    public void settings() {
+        size(800, 800);
+    }
+
+    public void setup() {
+
+    }
+
+    public void draw() {
+
+    }
+
 }
