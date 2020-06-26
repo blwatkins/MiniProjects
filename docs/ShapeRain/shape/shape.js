bd1889521e2b46cd2cf990e7bb3cd1fbade39a67
Brittni Watkins <blwatkins@smu.edu>
2020-06-26 15:41:01 -0500

    Add fade and isFaded methods to Shape class.

--- docs/ShapeRain/shape/shape.js
+++ docs/ShapeRain/shape/shape.js
@@ -2,6 +2,7 @@ class Shape {
 
     constructor(position) {
         this.position = createVector(position.x, position.y);
+        this.color = new Color(color(0, 0, 255));
     }
 
     display() {
@@ -10,5 +11,13 @@ class Shape {
 
     move() {
         this.position.y++;
+    }
+
+    fade() {
+        this.color.setAlpha(this.color.getAlpha() - 1);
+    }
+
+    isFaded() {
+        this.color.getAlpha() <= 0;
     }
 }
