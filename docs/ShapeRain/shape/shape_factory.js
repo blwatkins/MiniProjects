3908a64dde6bcdbd3ec666f72f611d6d84ab0e13
Brittni Watkins <blwatkins@smu.edu>
2020-06-28 08:20:12 -0500

    Add star production to shape factory. #12

--- docs/ShapeRain/shape/shape_factory.js
+++ docs/ShapeRain/shape/shape_factory.js
@@ -3,6 +3,7 @@ class ShapeFactory {
     constructor() {
         this._circle = 0;
         this._polygon = 1;
+        this._star = 2;
         this.type = this._circle;
         this.sideCount = 0;
         this.randomize();
@@ -13,19 +14,24 @@ class ShapeFactory {
 
         if (this.type === this._circle) {
             shape = new Circle(position, radius)
-        } else {
+        } else if (this.type === this._polygon) {
             shape = new Polygon(position, radius, this.sideCount);
+        } else {
+            shape = new Star(position, radius, this.sideCount);
         }
 
         return shape;
     }
 
     randomize() {
-        let r = int(random(200));
+        let r = int(random(300));
 
-        switch (r % 2) {
+        switch (r % 3) {
             case 1: 
                 this.type = this._circle;
+                break;
+            case 2:
+                this.type = this._star;
                 break;
             default:
                 this.type = this._polygon;
