9793a9fbe6f38af7effc1b71a144dc3ca967af61
Brittni Watkins <blwatkins@smu.edu>
2020-07-01 10:37:37 -0500

    Add Color instance variable

--- NoOverlap/src/Circle.java
+++ NoOverlap/src/Circle.java
@@ -1,21 +1,27 @@
 import processing.core.PApplet;
 import processing.core.PVector;
 
+import color.Color;
+
 public class Circle {
     private PApplet p;
     private PVector position;
     private float radius;
-    private int color;
+    private Color color;
 
     public Circle(PApplet p) {
         this.p = p;
         position = new PVector(p.random(p.width), p.random(p.height));
         radius = p.random(1, 50);
-        color = p.color((int)p.random(255), (int)p.random(255), (int)p.random(255));
+        color = new Color(p, p.color(255));
+    }
+
+    public void setColor(int color) {
+        this.color.setColor(color);
     }
 
     public void display() {
-        p.fill(color);
+        p.fill(color.getColor());
         p.noStroke();
         p.ellipse(position.x, position.y, radius * 2, radius * 2);
     }
