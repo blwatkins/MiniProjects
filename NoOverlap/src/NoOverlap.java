2281725def74ac131efdff5dc24551cbc041ca29
Brittni Watkins <blwatkins@smu.edu>
2020-07-01 10:06:47 -0500

    Remove old noOverlap algorithm

--- NoOverlap/src/NoOverlap.java
+++ NoOverlap/src/NoOverlap.java
@@ -21,8 +21,6 @@ public class NoOverlap extends PApplet {
         for (int i = 0; i < 200; i++) {
             circles.add(new Circle(this));
         }
-
-        checkOverlap();
     }
 
     public void draw() {
@@ -36,44 +34,5 @@ public class NoOverlap extends PApplet {
     public void mousePressed() {
         PVector position = new PVector(mouseX, mouseY);
         circles.add(new Circle(this, position));
-        checkOverlap();
     }
-
-    public void keyPressed() {
-        circles.add(new Circle(this));
-        checkOverlap();
-    }
-
-    private void checkOverlap() {
-        boolean overlap;
-
-        do {
-            overlap = false;
-            for (Circle a : circles) {
-
-                for (Circle b : circles) {
-
-                    if (a != b) {
-
-                        if (a.overlap(b)) {
-                            overlap = true;
-                            removeOverlap(a, b);
-                        }
-                    }
-                }
-            }
-        } while (overlap);
-    }
-
-    private void removeOverlap(Circle a, Circle b) {
-        PVector diff = PVector.sub(a.getPosition(), b.getPosition());
-
-        if (diff.mag() == 0) {
-            diff.set(0, 1);
-        }
-
-        diff.normalize().mult(a.getRadius() + b.getRadius());
-        b.getPosition().set(PVector.add(a.getPosition(), diff));
-    }
-
 }
