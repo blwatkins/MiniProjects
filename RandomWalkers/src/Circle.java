cc3bc4569e6c1fa7336c5130f7ad2dff04fdfe6a
Brittni Watkins <blwatkins@smu.edu>
2020-08-19 09:29:10 -0500

    Add constructor to Circle class

--- RandomWalkers/src/Circle.java
+++ RandomWalkers/src/Circle.java
@@ -1,11 +1,22 @@
 import processing.core.PApplet;
 import processing.core.PVector;
 
+import color.Color;
+
 public class Circle {
     private PApplet p;
     private PVector position;
     private PVector speed;
     private int maxSpeed;
     private float radius;
-    
+    private Color color;
+
+    public Circle(PApplet p, PVector position, int maxSpeed) {
+        this.p = p;
+        this.position = position.copy();
+        this.maxSpeed = maxSpeed;
+        speed = new PVector(0, 0);
+        radius = p.random(5, 50);
+        color = new Color(p, p.color(0, 0, 255));
+    }
 }
