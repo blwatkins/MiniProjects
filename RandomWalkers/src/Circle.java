3cb5e6017b8787277dc3dcd5029be4c40ef1d038
Brittni Watkins <blwatkins@smu.edu>
2020-08-19 09:25:36 -0500

    Add instance variables to Circle class

--- RandomWalkers/src/Circle.java
+++ RandomWalkers/src/Circle.java
@@ -1,2 +1,11 @@
+import processing.core.PApplet;
+import processing.core.PVector;
+
 public class Circle {
+    private PApplet p;
+    private PVector position;
+    private PVector speed;
+    private int maxSpeed;
+    private float radius;
+    
 }
