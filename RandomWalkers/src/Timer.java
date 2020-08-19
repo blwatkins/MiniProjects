c6bd58e7d967c0aa6973470a6713f2c43958cfb2
Brittni Watkins <blwatkins@smu.edu>
2020-08-19 09:49:27 -0500

    Add constructor and methods to Timer class

--- RandomWalkers/src/Timer.java
+++ RandomWalkers/src/Timer.java
@@ -1,2 +1,27 @@
+import processing.core.PApplet;
+
 public class Timer {
+    private final PApplet p;
+    private final int millisecondWait;
+    private int previousMilliseconds;
+
+    public Timer(PApplet p, int millisecondWait) {
+        this.p = p;
+        this.millisecondWait = millisecondWait;
+        previousMilliseconds = p.millis();
+    }
+
+    public boolean isReady() {
+        boolean isReady = false;
+
+        if (previousMilliseconds + millisecondWait <= p.millis()) {
+            isReady = true;
+        }
+
+        return isReady;
+    }
+
+    public void reset() {
+        previousMilliseconds = p.millis();
+    }
 }
