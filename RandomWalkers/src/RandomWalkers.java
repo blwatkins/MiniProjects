5b6b49281ecb48b023af0f305e6cc3816b69eec7
Brittni Watkins <blwatkins@smu.edu>
2020-08-19 08:37:45 -0500

    Add initial Processing code to RandomWalkers project

--- RandomWalkers/src/RandomWalkers.java
+++ RandomWalkers/src/RandomWalkers.java
@@ -1,2 +1,21 @@
-public class RandomWalkers {
+import processing.core.PApplet;
+
+public class RandomWalkers extends PApplet {
+
+    public static void main(String[] args) {
+        String[] processingArgs = {"RandomWalkers"};
+        PApplet.main(processingArgs);
+    }
+
+    public void settings() {
+        size(800, 800);
+    }
+
+    public void setup() {
+
+    }
+
+    public void draw() {
+
+    }
 }
